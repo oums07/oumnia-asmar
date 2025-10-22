417fa4af5ab88e23d63c03315f9c8265a619711b
oums07 <143686620+oums07@users.noreply.github.com>
2025-10-22 20:35:15 +0200

    Create sort.c

---
+++ TD2/exo2/sort.c
@@ -0,0 +1,150 @@
+#include <stdlib.h>
+#include "sort.h"
+#include "utils.h"
+
+void selection_sort(int *arr, int n)
+{   int min,temp;
+    // selection sort iterative
+    for(int i=0;i<i-2;i++){
+        min=i;
+        for(int j=i+1;j<j-1;j++)
+            min=j;
+        temp= arr[i];
+        arr[i]=arr[min];
+        arr[min]=temp;
+
+    }
+}
+
+void insertion_sort(int *arr, int n)
+{
+    // insertion sort iterative
+    int key,j;
+    for(int i=1;i<n-1;i++){
+        key=arr[i];
+        j=i-1;
+        while( j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+
+void bubble_sort(int *arr, int n)
+{
+    // bubble sort iterative
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1])
+               swap_int(&arr[j],&arr[j+1]);
+        }
+
+    }
+}
+
+void merge(int *arr, int *tmp, int l, int m, int r)
+{
+    // merge function
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int i;
+    int j;
+    int k;
+     for (i = 0; i < n1; i++){
+        tmp[i] = arr[l+i];
+    }
+    for  (j = 0; j < n2; j++){
+        tmp[j+m+1] = arr[m+1+j];
+    }
+
+    i = 0;
+    j = 0;
+    k = l;
+    
+    while (i<n1 && j<n2){
+        if (tmp[i+l] <= tmp[j+m+1]){
+            arr[k] = tmp[i+l];
+            i++;
+        }
+        else{
+            arr[k] = tmp[j+m+1];
+            j++;
+        }
+        k++;
+    }
+
+    while (i<n1){
+        arr[k] = tmp[l+i];
+        i++;
+        k++;
+    }
+    while (j<n2){
+        arr[k] = tmp[j+m+1];
+        j++;
+        k++;
+    }
+}
+
+void mergesort_rec(int *arr, int *tmp, int l, int r)
+{
+    // recursive steps of merge sort
+    if (l<r){
+        int mid = l + (r-l)/2;
+        mergesort_rec(arr, tmp, l, mid);
+        mergesort_rec(arr,tmp, mid+1,r);
+        merge(arr,tmp,l,mid,r);
+    }
+    
+}
+
+void merge_sort(int *arr, int n)
+{
+    if (!arr || n <= 1)
+        return;
+    int *tmp = (int *)malloc((size_t)n * sizeof(int));
+    if (!tmp)
+        return;
+    mergesort_rec(arr, tmp, 0, n - 1);
+    free(tmp);
+}
+int partition(int *arr, int l, int r)
+{
+    // partition function
+    int pivot = arr[r];
+    int i = l - 1;
+
+    for (int j = l; j<= r - 1; j++){
+        if (arr[j] < pivot){
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i+1], &arr[r]);
+    return i+1;
+
+}
+
+void quick_sort_rec(int *arr, int l, int r)
+{
+    if (l<r){
+        // recursive steps of quick sort
+        int part = partition(arr, l, r);
+
+        quick_sort_rec(arr, l , part -1);
+        quick_sort_rec(arr, part +1, r);
+    }
+}
+
+void quick_sort(int *arr, int n)
+{
+    if (!arr || n <= 1)
+        return;
+    quick_sort_rec(arr, 0, n - 1);
+}
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
